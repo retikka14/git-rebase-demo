4305990f391d3221aff1a79e5d29e0cc1165a4b5
G S RETIKKA PRASAD <retikkaprasad@gmail.com>
2026-09-09 08:57:13 +0530

    Add multiply function to calculator

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 def divide(a, b):
     return a / b
+
 
 print(add(10, 5))
